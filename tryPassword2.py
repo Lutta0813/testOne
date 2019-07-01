0fff67087727e5bc2f45e5aeda8abd9a8b71615e
LuTsungTa <steven82813@gmail.com>
2019-07-02 02:08:25 +0800

    more humane

--- tryPassword2.py
+++ tryPassword2.py
@@ -7,12 +7,14 @@ password = 'a123456'
 checkPasswordTimes = 3
 
 while checkPasswordTimes > 0:
+	checkPasswordTimes = checkPasswordTimes - 1
 	pwd = input('請輸入密碼： ')
 	if pwd == password:
 		print('登入成功！')
 		break
 	else:
-		checkPasswordTimes = checkPasswordTimes - 1
-		print('密碼輸入錯誤，還有', checkPasswordTimes, '次機會')
-		if checkPasswordTimes == 0:
-			print('密碼輸入錯誤達三次，帳號已鎖定')
+		print('密碼輸入錯誤!')
+		if checkPasswordTimes > 0:
+			print('還有', checkPasswordTimes, '次機會')
+		else:
+			print('密碼輸入錯誤達三次，帳號已鎖定。')
